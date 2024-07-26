93acecbd43cd1b929fa83431987a44e08708c59d
Carlos Buitrago <letscatchupagain@gmail.com>
2024-07-26 13:57:14 +0100

    added function that returns names of companies that issued invalid card numbers. Also created a file with all the variables to organise the code

--- script.test.js
+++ script.test.js
@@ -1,4 +1,8 @@
-const { validateCred, findInvalidCards } = require('./script.js');
+const {
+  validateCred,
+  findInvalidCards,
+  idInvalidCardCompanies,
+} = require('./script.js');
 
 describe('checks if number is valid', () => {
   it('should return true', () => {
@@ -19,6 +23,16 @@ describe('findInvalidCards()', () => {
     const valid = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
     const invalid = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
     const batch = [valid, invalid];
-    expect(findInvalidCards(batch)).toEqual([valid]);
+    expect(findInvalidCards(batch)).toEqual([invalid]);
+    expect(findInvalidCards(batch));
+  });
+
+  describe('idInvalidCardCompanies()', () => {
+    it('should find companies that issued the card, and should only be listed once', () => {
+      const valid = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
+      const invalid = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
+      const batch = [valid, invalid];
+      expect(idInvalidCardCompanies(batch)).toEqual(['Visa']);
+    });
   });
 });
